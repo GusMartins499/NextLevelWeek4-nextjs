989c24e41d11bd87e8db3a2acf9320084a5dba4b
Gusmartins499 <gustavosm994@gmail.com>
2021-03-02 22:12:40 -0300

    first commit

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,16 @@
+import styles from '../../styles/components/Profile.module.css';
+
+export default function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/gusmartins499.png" alt="Gustavo Martins" />
+      <div>
+        <strong>Gustavo Martins</strong>
+        <p>
+          <img src="icons/level.svg" alt="level" />
+          Level 1
+          </p>
+      </div>
+    </div>
+  );
+}

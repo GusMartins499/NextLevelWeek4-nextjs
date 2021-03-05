3be29e7f1659c9f24e374b9c59db5c0d09705cab
Gusmartins499 <gustavosm994@gmail.com>
2021-03-05 13:26:00 -0300

    created context

--- src/components/ChallengeBox/index.tsx
+++ src/components/ChallengeBox/index.tsx
@@ -1,24 +1,28 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../../contexts/ChallengesContext';
+
 import styles from '../../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-  const hasActiveChallenge = true;
+  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
 
   return (
     <div className={styles.challengeBoxContainer}>
-      { hasActiveChallenge ? (
+      { activeChallenge ? (
         <div className={styles.challengeActive}>
-          <header>Ganhe 400xp</header>
+          <header>Ganhe {activeChallenge.amount}</header>
 
           <main>
-            <img src="icons/body.svg" alt="Body" />
+            <img src={`icons/${activeChallenge.type}.svg`} alt={`${activeChallenge.type}`} />
             <strong>Novo desafio</strong>
-            <p>Levante e faça uma caminhada de 3 minutos</p>
+            <p>{activeChallenge.description}</p>
           </main>
 
           <footer>
             <button
               type="button"
-              className={styles.challengeFailedButton}>
+              className={styles.challengeFailedButton}
+              onClick={resetChallenge}>
               Falhei
               </button>
             <button
@@ -29,14 +33,14 @@ export function ChallengeBox() {
           </footer>
         </div>
       ) : (
-          <div className={styles.challengeNotActive}>
-            <strong>Finalize um ciclo para receber um desafio</strong>
-            <p>
-              <img src="icons/level-up.svg" alt="Level Up" />
+        <div className={styles.challengeNotActive}>
+          <strong>Finalize um ciclo para receber um desafio</strong>
+          <p>
+            <img src="icons/level-up.svg" alt="Level Up" />
           Avance de level completando desafios
         </p>
-          </div>
-        )}
+        </div>
+      )}
     </div>
   );
 }
